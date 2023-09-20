97fa58b1eb9616f1919cc7cd1eec3a682f4ed5ae
KitSarmiento <ksarmiento020@gmail.com>
2023-09-19 23:18:59 -0400

    Added a function to create and  generate the logo

--- index.js
+++ index.js
@@ -48,3 +48,51 @@ class Svg {
     return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
   }
 }
+
+//This function will initialize the app
+function init() {
+  console.log("Welcome to the Logo Generator!");
+
+  createLogo();
+}
+
+// Function to create the logo
+async function createLogo() {
+  try {
+    const answers = await inquirer.prompt(questions);
+
+    let shapeElement = "";
+
+    switch (answers.shape) {
+      case "Triangle":
+        shapeElement = `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+        break;
+
+      case "Circle":
+        shapeElement = `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
+        break;
+
+      case "Square":
+        shapeElement = `<rect x="73" y="40" width="160" height="120" fill="${answers.shapeColor}"/>`;
+        break;
+    }
+
+    const textElement = `<text x="10" y="100" fill="${answers.textColor}" font-size="30">${answers.text}</text>`;
+
+    const svg = new Svg();
+    svg.textElement = textElement;
+    svg.shapeElement = shapeElement;
+
+    const svgCode = svg.render();
+
+    // Save the SVG to a file
+    fs.writeFileSync("logo.svg", svgCode);
+
+    console.log("Generated logo.svg");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// Call the init function
+init();
